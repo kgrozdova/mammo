6446f6aa26c9838023c594e35b946b874650c488
kgrozdova <kgrozdova@gmail.com>
2014-11-17 14:23:07 +0000

    Create dailyCalibration.cpp

---
+++ dailyCalibration.cpp
@@ -0,0 +1,45 @@
+#include "main_header.h"
+#include <math.h>
+#include <string>
+#include <sstream>
+
+void dailyCalibration::insertFilTar(mammography mammData){
+    string filter = various::ToString<OFString>(mammData.Filter);
+    string target = various::ToString<OFString>(mammData.Target);
+    if(filter == "RHODIUM" && target == "RHODIUM"){
+        filTar = "RhRh";
+    } else if(filter == "MOLYBDENUM" && target == "MOLYBDENUM"){
+        filTar = "MoMo";
+    } else if(filter == "MOLYBDENUM" && target == "RHODIUM"){
+        filTar = "MoRh";
+    } else{
+        cerr << "Error in filter/target combination";
+    }
+}
+
+void dailyCalibration::InserQcTTg(mammography mammData, const string fileName){
+    ifstream ifs;
+    ifs.open(fileName, ifstream::in);
+    double MPV;
+    size_t found,found2;
+    int sizeStr;
+    string str, MPVStr, tStr, tgStr;
+    if(ifs.good()){
+        getline(ifs, str);
+        sizeStr = str.length();
+        found = str.find(",");
+        found2 = str.find(",",found+1);
+        MPVStr = str.substr(0, found);
+        istringstream i(MPVStr);
+        i >> MPV;
+        qc_ln_MPV_mAs = log(MPV/(double)mammData.Exposure);
+        tStr = str.substr(found+1, (int)found2-(int)found-1);
+        istringstream k(tStr);
+        k >> t;
+        tgStr = str.substr(found2, sizeStr-found2);
+        istringstream l(tgStr);
+        l >> tg;
+    }
+}
+
+
