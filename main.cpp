4dea5ea770a8a10345ea230fa4f6be6a9ad333eb
kgrozdova <kgrozdova@gmail.com>
2014-11-17 14:20:48 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,119 +1,163 @@
 #include "main_header.h"
+#include <map>
+#include <iterator>
+#include <vector>
+typedef map< string, phantomCalibration> calibData;
 
 int main(){
+
     mammography mammData;
-    mammData.mammography::loadHeaderData("/home/ksenia/Documents/Man_4/MPhys_mammography/Week_2/FFDM1/Control_prior_anon-00001-LMLO111105-20110328-RAW.dcm");
-    mammData.mammography::loadPixelData("/home/ksenia/Documents/Man_4/MPhys_mammography/Week_2/FFDM1/Control_prior_anon-00001-LMLO111105-20110328-RAW.dcm");
-    int kV = (int)mammData.XRayTubeCurrent;
+    mammData.mammography::loadHeaderData("Control_prior_anon-00001-LMLO111105-20110328-RAW.dcm");
+    // load data from processed DICOM file
+    mammData.mammography::loadPixelData("Control_prior_anon-00001-LMLO111105-20110328-RAW.dcm");
+//    // convert dicom to jpeg
+//    char* DICOMfile = "Control_prior_anon-00001-LMLO111105-20110328-RAW.dcm";
+//    char* JPEGfile = "Control_prior_anon-00001-LMLO111105-20110328-RAW.jpeg";
+//    char command[50];
+//    sprintf(command, "dcmj2pnm %s %s +oj", DICOMfile, JPEGfile);
+//    system(command);
+
+    //                  //
+    // Image processing //
+    //                  //
+    #define OL_WIDTH 512
+    #define OL_EXT_LENGTH 4
+    #define OL_DRAW
+    #ifdef OL_DRAW
+    //	#define OL_DRAW_HIST
+    #define OL_DRAW_THRESH
+    #define OL_DRAW_DIST
+    //	#define OL_DRAW_DISTMAP
+    //	#define OL_DRAW_CORNER
+    #endif
+    //cv::Mat mMammo = breast::getMat(JPEGfile);
+    cv::Mat mMammo((int)mammData.Rows, (int)mammData.Columns, CV_16UC1, 0);
+    for(int i = 0; i < mammData.Rows; i++){
+        for(int j = 0; j < mammData.Columns; j++){
+             //mMammo.at<int>(i,j)= mammData.pixelArr[i+(int)mammData.Columns*j];
+        }
+    }
+    string strFileName = breast::fileNameErase("Control_prior_anon-00001-LMLO111105-20110328-PROCESSED.dcm");
+
+    int iCOLOUR_MAX = breast::getBitDepth(mMammo);
+
+    //
+    // SEPERATING THE BREAST FROM THE BACKGROUND
+    //
+    vector<cv::Mat> bgr_planes = breast::separate3channels(mMammo);
+    // Establish the number of bins
+	int histSize = iCOLOUR_MAX;
+
+	// Set the ranges (for B,G,R) )
+	float range[] = {0, float(iCOLOUR_MAX)+1} ;
+	const float* histRange = {range};
+	// Set histogram behaviour.
+	bool uniform = true; bool accumulate = false;
+	cv::Mat mHistB;
+
+	// Compute the histogram.
+	cv::calcHist(&bgr_planes[0], 1, 0, cv::Mat(), mHistB, 1, &histSize, &histRange, uniform, accumulate );
+    breast::drawHist(histSize);
+
+    pair<float, float> iNBin = breast::findPeak(mHistB, histSize);
+    float iQuartMax = breast::findWidth(iNBin.second, iNBin.first, mHistB);
+
+    //
+    //FINDING THE CONTOUR WHICH THE DESRIBES THE EDGE OF THE BREAST
+    //
+    cv::Mat mMammoThreshed;
+	// Threshold the image to 'cut off' the brighter peak from the histogram.
+	cv::threshold(mMammo, mMammoThreshed, iCOLOUR_MAX*(iQuartMax/histSize), iCOLOUR_MAX, 1);
+	// MAGIC
+	cv::Mat	mMammoThreshedCopy = mMammoThreshed;
+    //
+    // DISTANCE TRANSFORM
+    //
+
+    // Convert the threshold into greyscale to stop the distance transform complaining. Consider moving this to the start of the programme.
+	cv::cvtColor(mMammoThreshed,mMammoThreshed, cv::COLOR_BGR2GRAY);
+	// Use a less accurate but smoother looking distance transform. More research needed here
+	cv::Mat mMammoDist;
+	cv::distanceTransform(mMammoThreshed, mMammoDist, cv::DIST_L2, cv::DIST_MASK_PRECISE, CV_32F);
+    cv::Mat mMammoThreshedCont;
+	mMammoThreshed.convertTo(mMammoThreshedCont, CV_8U);
+	vector<cv::Point> pEdgeContour = breast::distanceTransform(mMammoThreshed);
+
+	//
+	// FIGURE OUT WHETHER WE ARE LOOKING AT A LEFT OR RIGHT BREAST
+	//
+	vector<cv::Point> pEdgeContourCopy = pEdgeContour;
+	bool bLeft = breast::leftOrRight(pEdgeContour, mMammo);
+	 //
+	 // TRY TO FIND A CORNER NEAR THE BOTTOM OF THE BREAST
+	 //
+
+    // Make a probability map of likely corners in the mammogram.
+	cv::Mat mCorner;
+	cv::cornerHarris(mMammoThreshed, mCorner, 40, 3, 0.04);
+	float iMax = breast::findIMax(mCorner);
+	// Threshold this map repeatedly until we find at least three probable regions.
+    cv::Mat mCornerThresh;
+	float iDivisor = 1.25;
+	vector<vector<cv::Point>> pContours = breast::findCorners(iDivisor, mCorner, iMax, iCOLOUR_MAX);
+		// Find the most spatially spread out corner. Unused, currently - what does it mean?
+	sort(pContours.begin(),pContours.end(),[]
+		(const vector<cv::Point> &l, const vector<cv::Point> &r){
+			return cv::contourArea(l) > cv::contourArea(r);
+		});
+    // Find the centers of the corners.
+    vector<cv::Point> vecContCents = breast::findCornerCentre(pContours);
+
+    // Pick a corner to cut off.
+    pair<int, int> iContPos = breast::pickCornerCutOff(mMammo, pContours, vecContCents, bLeft);
+    //
+    // PAINT OVER UNINTERESTING PARTS OF THE BREAST ON THE THRESHOLDED IMAGE
+    //
+
+    breast::deleteUnneeded(mMammo, pEdgeContourCopy, bLeft, mMammoThreshed, mMammoThreshedCopy, iContPos.second);
+
+    //
+    // FINDING THE BREAST THICKNESS
+    //
+
+    // Normalise the distance map to fit onto our graph.
+    cv::normalize(mMammoDist, mMammoDist, 0, 255, cv::NORM_MINMAX, -1, cv::Mat());
+    vector<float> vecDistBright = breast::getDistBright(mMammoDist, mMammo);
+
+    int dist_w = histSize*2; int dist_h = 512;
+    cv::Mat distImage(dist_h, dist_w, CV_8UC3, cv::Scalar(0,0,0));
+    cv::Mat_<int> mMammoDistChar = mMammoDist;
+    cv::Mat mMammoCopy;
+    cv::cvtColor(mMammo, mMammoCopy, cv::COLOR_BGR2GRAY);
+    vector<float> vecDistBrightBrightest = breast::brestThickness(mMammo, mMammoDist, vecDistBright, histSize,
+                                                                    mMammoDistChar, mMammoCopy);
+
+    //
+    // DRAWING THE PICTURES
+    //
+    breast::drawImages(vecDistBrightBrightest, strFileName, distImage, mCornerThresh, mMammoDist, mMammoThreshedCopy, mMammo, histSize);
+    string KVP = various::ToString<OFString>(mammData.KVP);
+    string bodyThickness = various::ToString<OFString>(mammData.BodyPartThickness);
+    int thickness = atoi(bodyThickness.c_str());
+    int thickArr[2];
+    div_t divresult;
+    divresult = div(thickness,10);
+    thickArr[0] = divresult.quot*10;
+    thickArr[1] = (divresult.quot+1)*10;
     dailyCalibration dcalib;
-    dcalib.qc_ln_MPV_mAs = 3.5;
-    dcalib.tg = 5;
-    if(dcalib.filTar == "MoMo"){
-        phantomCalibration* t20_MoMo;
-        t20_MoMo->getThicknessData("av_calib_data.xls", "MoMo", kV, 2, 6);
-        t20_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t30_MoMo;
-        t30_MoMo->getThicknessData("av_calib_data.xls", "MoMo", kV, 7, 13);
-        t30_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t40_MoMo;
-        t40_MoMo->getThicknessData("av_calib_data.xls", "MoMo", kV, 14, 20);
-        t40_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t50_MoMo;
-        t50_MoMo->getThicknessData("av_calib_data.xls", "MoMo", kV, 21, 23);
-        t50_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t60_MoMo;
-        t60_MoMo->getThicknessData("av_calib_data.xls", "MoMo", kV, 24, 26);
-        t60_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t70_MoMo;
-        t70_MoMo->getThicknessData("av_calib_data.xls", "MoMo", kV, 27, 29);
-        t70_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-    }
-    if(dcalib.filTar == "MoRh"){
-        phantomCalibration* t20_MoMo;
-        t20_MoMo->getThicknessData("av_calib_data.xls", "MoRh", kV, 2, 6);
-        t20_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t30_MoMo;
-        t30_MoMo->getThicknessData("av_calib_data.xls", "MoRh", kV, 7, 13);
-        t30_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t40_MoMo;
-        t40_MoMo->getThicknessData("av_calib_data.xls", "MoRh", kV, 14, 20);
-        t40_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t50_MoMo;
-        t50_MoMo->getThicknessData("av_calib_data.xls", "MoRh", kV, 21, 25);
-        t50_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t60_MoMo;
-        t60_MoMo->getThicknessData("av_calib_data.xls", "MoRh", kV, 26, 30);
-        t60_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t70_MoMo;
-        t70_MoMo->getThicknessData("av_calib_data.xls", "MoRh", kV, 31, 31);
-        t70_MoMo->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-    }
-    if(dcalib.filTar == "MoRh"){
-        phantomCalibration* t20_MoRh;
-        t20_MoRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 2, 6);
-        t20_MoRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t30_MoRh;
-        t30_MoRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 7, 13);
-        phantomCalibration* t40_MoRh;
-        t30_MoRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        t40_MoRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 14, 20);
-        phantomCalibration* t50_MoRh;
-        t40_MoRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        t50_MoRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 21, 25);
-        phantomCalibration* t60_MoRh;
-        t50_MoRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        t60_MoRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 26, 30);
-        phantomCalibration* t70_MoRh;
-        t60_MoRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        t70_MoRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 31, 31);
-        t70_MoRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-    }
-    if(dcalib.filTar == "RhRh" && kV == 29){
-        phantomCalibration* t05_RhRh;
-        t05_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 2, 3);
-        t05_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t10_RhRh;
-        t10_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 4, 6);
-        t10_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t20_RhRh;
-        t20_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 7, 11);
-        t20_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t30_RhRh;
-        t30_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 12, 18);
-        t30_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t40_RhRh;
-        t40_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 19, 27);
-        t40_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t50_RhRh;
-        t50_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 28, 38);
-        t50_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t60_RhRh;
-        t60_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 39, 51);
-        t60_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t65_RhRh;
-        t65_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 52, 65);
-        t65_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t70_RhRh;
-        t70_RhRh->getThicknessData("av_calib_data.xls", "RhRh", kV, 66, 80);
-        t70_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-    } else if(dcalib.filTar == "RhRh"){
-        phantomCalibration* t20_RhRh;
-        t20_RhRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 2, 4);
-        t20_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t30_RhRh;
-        t30_RhRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 5, 7);
-        t30_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t40_RhRh;
-        t40_RhRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 8, 14);
-        t40_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t50_RhRh;
-        t50_RhRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 15, 19);
-        t50_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t60_RhRh;
-        t60_RhRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 20, 22);
-        t60_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-        phantomCalibration* t70_RhRh;
-        t70_RhRh->getThicknessData("av_calib_data.xls", "MoRh", kV, 23, 25);
-        t70_RhRh->dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs);
-    }
+    dcalib.insertFilTar(mammData);
+    dcalib.InserQcTTg(mammData, "qc.dat");
+    calibData dat;
+    phantomCalibration calib1;
+    calib1 = phantomCalibration::getThicknessData(dcalib.filTar, atoi(KVP.c_str()), thickArr[0]);
+    calib1.phantomCalibration::dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs, atoi(KVP.c_str()), dcalib.filTar, dcalib.t);
+    dat["lower"] = calib1;
+    phantomCalibration calib2;
+    calib2 = phantomCalibration::getThicknessData(dcalib.filTar, atoi(KVP.c_str()), thickArr[1]);
+    calib2.phantomCalibration::dataCorrection(dcalib.tg, dcalib.qc_ln_MPV_mAs, atoi(KVP.c_str()), dcalib.filTar, dcalib.t);
+    dat["higher"] = calib2;
+    int tg = breast::fibrogland(mammData,dat);
+    int t = breast::totalBreast(mammData);
+    double glandPercent = breast::glandpercent(tg, t);
+    cout << glandPercent << endl;
 }
